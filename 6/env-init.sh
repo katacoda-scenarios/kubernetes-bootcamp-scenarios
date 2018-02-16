1beef1622382b4dd3285804024b9b8a4934abe1d
Ben Hall <ben@benhall.me.uk>
2018-02-16 23:17:41 +0000

    Move to Minikube 1.9

--- 6/env-init.sh
+++ 6/env-init.sh
@@ -1,4 +1,1 @@
-~/.bin/minikube start
-kubectl run kubernetes-bootcamp --image=docker.io/jocatalin/kubernetes-bootcamp:v1 --port=8080 --replicas=4
-kubectl expose deployments/kubernetes-bootcamp --type="NodePort" --port 8080
-echo "done" >> /opt/katacoda-completed
+ssh root@host01 "while [ \`docker ps | wc -l\` -eq 1 ]; do sleep 1; done && kubectl run kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1 --port=8080 --replicas=4 && kubectl expose deployments/kubernetes-bootcamp --type=\"NodePort\" --port 8080 && echo done >> /opt/katacoda-completed"
