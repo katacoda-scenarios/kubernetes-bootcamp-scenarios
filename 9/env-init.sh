73c001642f08a38ad215149fc37f4cb101b3629d
Ben Hall <Ben@Benhall.me.uk>
2020-10-06 09:56:09 +0100

    Update env-init.sh

--- 9/env-init.sh
+++ 9/env-init.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/jamiecoleman92/guide-kubernetes-microprofile-config.git
-docker pull open-liberty:latest
+ssh root@host01 "git clone https://github.com/jamiecoleman92/guide-kubernetes-microprofile-config.git"
+ssh root@host01 "docker pull open-liberty:latest"
 
 ssh root@host01 "while [ \`minikube status &>/dev/null; echo \$?\` -ne 0 ]; do sleep 1; done && kubectl run kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1 --port=8080 && echo done >> /opt/katacoda-completed"
