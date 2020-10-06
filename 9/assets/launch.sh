443350cbe947f22f4dee4f6036667dea43550608
Ben Hall <Ben@Benhall.me.uk>
2020-10-06 09:40:30 +0100

    Update launch.sh

--- 9/assets/launch.sh
+++ 9/assets/launch.sh
@@ -1,3 +1,3 @@
-echo "Starting Kubernetes 1.15.0. This may take a few moments, please wait..."
+echo "Starting Kubernetes. This may take a few moments, please wait..."
 while [ `minikube status &>/dev/null; echo $?` -ne 0 ]; do sleep 1; done
 echo "Kubernetes Started"
