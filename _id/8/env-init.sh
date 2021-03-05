325e2498281452d4cb2c28d280802a425aa624bc
Ben Hall <ben@benhall.me.uk>
2021-03-05 08:21:45 +0000

    Fix bad find/replace

--- _id/8/env-init.sh
+++ _id/8/env-init.sh
@@ -1,1 +1,1 @@
-ssh root@host01 "while [ \`minikube status &>/dev/null; echo \$?\` -ne 0 ]; do sleep 1; done && kubectl create deployment kubernetes-bootcamp2 --image=gcr.io/google-samples/kubernetes-bootcamp:v1 --port=8080 && echo done >> /opt/katacoda-completed"
+ssh root@host01 "while [ \`minikube status &>/dev/null; echo \$?\` -ne 0 ]; do sleep 1; done && kubectl create deployment kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1 --port=8080 && echo done >> /opt/katacoda-completed"
