aaf18eb164aa687493bf2a3e60ea52d8ed6a0697
Ben Hall <ben@benhall.me.uk>
2021-03-05 08:21:45 +0000

    Fix bad find/replace

--- 9/env-init.sh
+++ 9/env-init.sh
@@ -1,4 +1,4 @@
 ssh root@host01 "git clone https://github.com/jamiecoleman92/guide-kubernetes-microprofile-config.git"
 ssh root@host01 "docker pull open-liberty:latest"
 
-ssh root@host01 "while [ \`minikube status &>/dev/null; echo \$?\` -ne 0 ]; do sleep 1; done && kubectl create deployment kubernetes-bootcamp2 --image=gcr.io/google-samples/kubernetes-bootcamp:v1 --port=8080 && echo done >> /opt/katacoda-completed"
+ssh root@host01 "while [ \`minikube status &>/dev/null; echo \$?\` -ne 0 ]; do sleep 1; done && kubectl create deployment kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1 --port=8080 && echo done >> /opt/katacoda-completed"
